75657aa274f8e1ddfc7cd3869470498234703eb6
JimM517 <80083198+JimM517@users.noreply.github.com>
2021-08-22 15:50:31 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const pokedex = document.getElementById('pokedex');
+
+const getPoke = () => {
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    };
+
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            moves: data.moves.map(move => move.move.name).join(', '),
+            stats: data.stats.map(stat => stat.stat.name).join(', '),
+            type: data.types.map(type => type.type.name).join(', '),
+        }));
+       getSprite(pokemon);
+    });
+};
+
+const getSprite = (pokemon) => {
+    const pokeHTML = pokemon.map(pokemon => `
+    <li class="card">
+        <img class="card-image" src="${pokemon.image}" />
+        <h2 class="card-title">${pokemon.id}.${pokemon.name}</h2>
+        <p class="card-subtitle">${pokemon.moves}</p>
+        <p class="card-subtitle">${pokemon.stats}</p>
+        <p class="card-subtitle">${pokemon.type}</p>
+    </li>
+    `)
+    pokedex.innerHTML = pokeHTML;
+}
+
+getPoke();
+
+
